e28ee828254dc8d5fe72b9075965dbaeee1bb905
John Gallion <johngallion@Johns-MacBook-Air.local>
2023-09-02 13:00:24 -0400

    changed format added links to conference pages and started api

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-document.getElementById("createTableBtn").addEventListener("click", function () {
-    // Create a new table element
-    var table = document.createElement("table");
+const ticker = document.getElementById('ticker');
 
-    // Create a table row
-    var row = table.insertRow(0);
+// Function to fetch sports updates from the backend (mock API)
+async function fetchSportsUpdates() {
+    try {
+        // Replace this with a real API endpoint in a production environment
+        const response = await fetch('https://api.collegefootballdata.com/games?year=2023&week=1&seasonType=regular&division=fbs');
+        const data = await response.json();
 
-    // Create a table cell
-    var cell = row.insertCell(0);
+        // Display the latest update in the ticker
+        if (data && data.length > 0) {
+            const latestUpdate = data[0];
+            ticker.textContent = `Latest Update: ${latestUpdate.text}`;
+        }
+    } catch (error) {
+        console.error('Error fetching sports updates:', error);
+    }
+}
 
-    // Add content to the cell
-    cell.innerHTML = `<div class="border border-secondary"><input type="text" ></input></div>`;
+// Fetch sports updates every 30 seconds (adjust the interval as needed)
+setInterval(fetchSportsUpdates, 30000);
 
-    // Add the table to the container
-    document.getElementById("tableContainer").appendChild(table);
-});
+// Initial fetch
+fetchSportsUpdates();
